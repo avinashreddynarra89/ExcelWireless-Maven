6b71e3ed4b0d547ff135d7a19e4e79e1eba112d5
alokpatel <alokpatel9824107152@gmail.com>
2017-04-10 22:43:41 -0400

    sd

--- src/main/java/com/excelwireless/ExcelWirelessMavenApplication.java
+++ src/main/java/com/excelwireless/ExcelWirelessMavenApplication.java
@@ -2,9 +2,8 @@ package com.excelwireless;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ExcelWirelessMavenApplication {
 
 	public static void main(String[] args) {
